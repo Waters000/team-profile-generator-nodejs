ce11189f444b674c1ca7cc4577a532f505010e34
Chris Waters <floridarealty001@gmail.com>
2022-02-28 17:46:25 -0500

    updated

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,20 @@
 
-const { expect } = require('jest');
-const Employee = require('../lib/Employee.js');
+const { expect } = require("@jest/globals");
+const Employee = require("../lib/Employee");
 
-jest.mock('..')
+//name,email,id,role validation
 
 
-test("creates an employee name", () => {
-    const employeeName = new Employee("chris");
-    expect(employeeName.name).toEqual("chris")
+describe('Employee', () => {
+    it('employee returns the info on the Employee oop constructor', () => {
+        const employee = new Employee("Chris", "55", "Floridarealty001@gmail.com", "Manager")
+        expect(employee.name).toEqual(expect.any(String))
+        expect(employee.employeeid).toEqual(3)
+        expect(employee.email).toEqual("Floridarealty001@gmail.com")
+        expect(employee.position).toEqual("Manager")
+    })
+
+   
+   
+
 })
